9bcb23a93bc98c561fcd0612a83ce0b5f5c4cdc4
Egba Frank <Egbafrank72@gmail.com>
2024-06-06 12:56:58 +0000

    Improved code efficiency, task 1, task 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL || new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
@@ -21,10 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
-	{
 		parent->left->parent = new_node;
-		new_node->left = parent->left;
-	}
+
 	parent->left = new_node;
 
 	return (new_node);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL || new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
@@ -21,10 +21,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->right = parent->right;
 
 	if (parent->right != NULL)
-	{
 		parent->right->parent = new_node;
-		new_node->right = parent->right;
-	}
+
 	parent->right = new_node;
 
 	return (new_node);
