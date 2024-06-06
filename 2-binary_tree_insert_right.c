a7d4d0adcebbdd758c039a9c072eb447d7eb8179
Egba Frank <Egbafrank72@gmail.com>
2024-06-06 13:00:42 +0000

    Fixed potential mem leaks, task 1, task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,9 +10,12 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL || new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
