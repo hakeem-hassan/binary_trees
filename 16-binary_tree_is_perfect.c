26e77e08826ec0480e760f16d958ea868e1260d8
Egba Frank <Egbafrank72@gmail.com>
2024-06-06 23:46:47 +0000

    Upated height logic

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -41,7 +41,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0, left_height = 0;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	if (tree->right != NULL)
 		right_height = 1 + binary_tree_height(tree->right);
