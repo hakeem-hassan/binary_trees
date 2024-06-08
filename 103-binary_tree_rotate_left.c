a0d1b5643bee71be60ed2bb1efea81fa9bf9c605
Egba Frank <Egbafrank72@gmail.com>
2024-06-08 14:38:14 +0000

    Added advanced tasks, -tasks 103 & 104

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: pointer to the root node to be performed on
+ *
+ * Return: pointer to the new root node
+ */
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *new_root = tree->right;
+
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+
+	if (new_root->parent != NULL)
+	{
+		if (new_root->parent->right == tree)
+			new_root->parent->right = new_root;
+
+		else if (new_root->parent->left == tree)
+			new_root->parent->left = new_root;
+	}
+	tree->right = new_root->left;
+
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
+
+	new_root->left = tree;
+
+	return (new_root);
+}
---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
+ * @tree: pointer to the root node to be performed on
+ *
+ * Return: pointer to the new root node
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root = tree->left;
+
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+
+	/* Update parent pointers */
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+
+	/* Update the parent's child pointer if tree was not the root */
+	if (new_root->parent != NULL)
+	{
+		if (new_root->parent->right == tree)
+			new_root->parent->right = new_root;
+
+		else if (new_root->parent->left == tree)
+			new_root->parent->left = new_root;
+	}
+
+	/* Perform the rotation */
+	tree->left = new_root->right;
+
+	if (new_root->right != NULL)
+		new_root->right->parent = tree;
+
+	new_root->right = tree;
+
+	return (new_root);
+}
